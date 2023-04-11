8783c0c8d4463e3a198b2a43491e29602668aab8
주성돈 <a53173956@gmail.com>
2023-04-11 06:57:42 +0000

    dsfsdf

---
+++ 47.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+
+     int num1 = 14;
+	 int num2 = 23
+	 int sum = 0; //초기화
+
+     sum = num1 + num2;
+	 printf("%d + %d = %d \n", num1, num2, sum);
+	
+	return 0;
+}
