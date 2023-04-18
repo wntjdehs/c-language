eba57f759cfc4f5306c3b991bf1c32638a555dd4
주성돈 <a53173956@gmail.com>
2023-04-18 07:28:20 +0000

    hjgjh

---
+++ 58.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	
+	
+	printf("%d %d\n", 3==2, 3!=2);
+	printf("%d %d\n", 3>2, 3<=2);
+	printf("%d \n", (3>2) && (3<=2) );
+	printf("%d \n", (3>2) ㅣㅣ(3<=2) );
+	printf("%d \n", !(3>2) );
+	
+	return 0;
+}
---
+++ 59.c
@@ -0,0 +1,8 @@
+ #include <stdio,h>
+ main()
+ {
+	 printf("%d %d\n", 3&1, 3ㅣ1);
+	 printf("%d\n", 3^1);
+	 printf("%d %d\n",3<<1, 3>>1);
+	 printf("%d\n", ~3);
+ }
